6aac4475dd4e7de8f9012d9ed83ed7ecfc7df8a6
unknown <wilverhidalgo@outlook.com>
2019-06-25 12:08:29 -0400

    add firebase config

--- superAdmin/src/app/app.module.ts
+++ superAdmin/src/app/app.module.ts
@@ -5,15 +5,30 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBLJBVtNqPx6EC0jPOhTXxzAortkXaDBk8",
+    authDomain: "tecnologiasinternet-745d3.firebaseapp.com",
+    databaseURL: "https://tecnologiasinternet-745d3.firebaseio.com",
+    projectId: "tecnologiasinternet-745d3",
+    storageBucket: "tecnologiasinternet-745d3.appspot.com",
+    messagingSenderId: "596202129245",
+    appId: "1:596202129245:web:1aeba3b44305d5e1"
+  }
+};
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
     IonicModule.forRoot(),
     AppRoutingModule
   ],
