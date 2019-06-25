94d8a1180be3fc5d1b12985d4e7d40378da95ee0
unknown <wilverhidalgo@outlook.com>
2019-06-25 15:06:37 -0400

    logintrue

--- superAdmin/src/app/adminuser/create-user/create-user.page.ts
+++ superAdmin/src/app/adminuser/create-user/create-user.page.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AngularFireDatabase} from '@angular/fire/database';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.page.html',
   styleUrls: ['./create-user.page.scss'],
 })
 export class CreateUserPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+ usuario:Usuarios;
+ db$: any;
+  constructor(private db: AngularFireDatabase,private router: Router) { 
   }
 
+  ngOnInit() {
+    this.db$ = this.db.list('/usuarios');
+  }
+  register(forms){
+       this.usuario=forms.value;
+       this.usuario.nivel=2;
+       delete this.usuario.password;
+       this.db$.push(this.usuario).then(data=>{
+        this.router.navigate(['list-user']);
+       });
+
+	
+  }
 }
